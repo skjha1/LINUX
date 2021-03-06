acab9b5cdcd0ea3fbb37911b2b592004afd640ce
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-06 11:34:04 +0530

    Create 03 Navigating filesystem.sh

---
+++ Basics Commands/03 Navigating filesystem.sh
@@ -0,0 +1,120 @@
+• pwd - present working directory 
+• cd- change directory 
+• ls , finding hidden folder with ls - list files
+• mkdir , rmdir - make dir , remove dir 
+• cp, rm, mv, locate - copy, move, remove , 
+• updatedb
+• passwd - changing the pswd 
+• man
+• -- help
+• man ls 
+
+
+-------------------------------------------------------------------------------------------------------------
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~/Documents/shivendra]
+└─# history                                                                                                                                                                                                                            130 ⨯
+    1  user
+    2  uname
+    3  uname -a
+    4  pwd
+    5  cd
+    6  pwd
+    7  cd ..
+    8  pwd
+    9  ls
+   10  cd root
+   11  ls
+   12  cd /etc/
+   13  ls
+   14  ls Desktop/
+   15  ls root
+   16  cd root
+   17  cd root/
+   18  cd root
+   19  cd ..
+   20  cd root/
+   21  ls
+   22  ls Desktop
+   23  ls
+   24  ls Desktop
+   25  pwd
+   26  cd Desktop
+   27  ls
+   28  cd -/Music/
+   29  cd /Music/
+   30  cd Desktop/
+   31  cd ..
+   32  cd Desktop
+   33  ls
+   34  cd ..
+   35  cd Pictures
+   36  ls
+   37  cd ..
+   38  cd Public
+   39  ls
+   40  cd ..
+   41  ls
+   42  cd Documents
+   43  ls
+   44  mkdir shivendra
+   45  ls
+   46  cd ..
+   47  ls
+   48  mkdir Shiva
+   49  ls
+   50  rmdir Shiva
+   51  ls
+   52  ls -la
+   53  cd .cache
+   54  ls
+   55  cd ..
+   56  ls
+   57  echo "HI this is shivendra " > test.txt
+   58  ls
+   59  cp text.txt Downloads
+   60  ls
+   61  ls Downloads
+   62  ls
+   63  cd ..
+   64  cd Downloads/
+   65  ls
+   66  cd ..
+   67  ls 
+   68  cd ..
+   69  cd root/
+   70  ls
+   71  ls Downloads
+   72  ls
+   73  cd Downloads
+   74  ls
+   75  cp text.txt Downloads/
+   76  cp test.txt Downloads/
+   77  ls
+   78  cd ..
+   79  ls
+   80  mv test.txt Downloads
+   81  ls
+   82  cd Downloads
+   83  ls
+   84  rm test.txt
+   85  ls
+   86  cd ..
+   87  ls
+   88  locate bash
+   89  updatedb
+   90  passwd
+   91  man ls
+   92  ls --help
+   93  ls
+   94  mkdir shiv
+   95  ls
+   96  rm shiv
+   97  rmdir shiv
+   98  ls 
+   99  cd Documents
+  100  echo "HI this is shivendra " > text.txt
+  101  ls
+  102  cd shivendra
+  103  ls
+
