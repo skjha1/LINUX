870f0e3087cc7e19f977f351a7ed19065b734efb
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-07 09:39:25 +0530

    Create 04 User and Privileges.sh

---
+++ Basics Commands/04 User and Privileges.sh
@@ -0,0 +1,346 @@
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# ls -la                                                              
+total 132
+drwx------ 14 root root  4096 Mar  6 21:51 .
+drwxr-xr-x 19 root root  4096 Feb 25 03:37 ..
+-rw-r--r--  1 root root  4705 Feb 25 03:32 .bashrc
+drwx------  6 root root  4096 Mar  6 20:51 .cache
+drwx------ 13 root root  4096 Mar  5 23:24 .config
+drwxr-xr-x  2 root root  4096 Mar  6 21:25 Desktop
+-rw-r--r--  1 root root    55 Mar  5 01:40 .dmrc
+drwxr-xr-x  3 root root  4096 Mar  5 23:53 Documents
+drwxr-xr-x  2 root root  4096 Mar  5 23:46 Downloads
+-rw-r--r--  1 root root     0 Mar  6 21:58 .face
+lrwxrwxrwx  1 root root    11 Feb 25 03:41 .face.icon -> /root/.face
+drwx------  3 root root  4096 Mar  6 20:51 .gnupg
+-rwxrwxrwx  1 root root     0 Mar  6 21:57 hello.txt
+-rw-------  1 root root     0 Mar  5 01:40 .ICEauthority
+drwxr-xr-x  3 root root  4096 Mar  5 01:40 .local
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Music
+drwxr-xr-x  2 root root  4096 Mar  5 23:49 Pictures
+-rw-r--r--  1 root root   148 Feb 16 04:16 .profile
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Public
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Templates
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-clipboard.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-display-svga-x11.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-draganddrop.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-seamless.pid
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Videos
+-rw-------  1 root root    49 Mar  6 20:51 .Xauthority
+-rw-------  1 root root  7413 Mar  6 21:59 .xsession-errors
+-rw-------  1 root root 12703 Mar  6 00:15 .xsession-errors.old
+-rw-------  1 root root   881 Mar  6 00:13 .zsh_history
+-rw-r--r--  1 root root  8381 Feb 25 03:32 .zshrc
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# ls -la /tmp/
+total 60
+drwxrwxrwt 13 root root 4096 Mar  6 21:45 .
+drwxr-xr-x 19 root root 4096 Feb 25 03:37 ..
+drwxrwxrwt  2 root root 4096 Mar  6 20:42 .font-unix
+drwxrwxrwt  2 root root 4096 Mar  6 20:51 .ICE-unix
+drwx------  2 root root 4096 Mar  6 20:51 ssh-sSuY8bezb2Ki
+drwx------  3 root root 4096 Mar  6 20:51 systemd-private-6c0e6b5fc30a40acb4665b9dc1a1f8e4-colord.service-Q6a71i
+drwx------  3 root root 4096 Mar  6 20:42 systemd-private-6c0e6b5fc30a40acb4665b9dc1a1f8e4-haveged.service-uL66Th
+drwx------  3 root root 4096 Mar  6 20:42 systemd-private-6c0e6b5fc30a40acb4665b9dc1a1f8e4-ModemManager.service-QzBTSg
+drwx------  3 root root 4096 Mar  6 20:42 systemd-private-6c0e6b5fc30a40acb4665b9dc1a1f8e4-systemd-logind.service-2KwCbg
+drwx------  3 root root 4096 Mar  6 20:51 systemd-private-6c0e6b5fc30a40acb4665b9dc1a1f8e4-upower.service-9UxClj
+drwxrwxrwt  2 root root 4096 Mar  6 20:42 .Test-unix
+-r--r--r--  1 root root   11 Mar  6 20:42 .X0-lock
+drwxrwxrwt  2 root root 4096 Mar  6 21:45 .X11-unix
+-rw-------  1 root root  394 Mar  6 20:51 .xfsm-ICE-O1XUZ0
+drwxrwxrwt  2 root root 4096 Mar  6 20:42 .XIM-unix
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# echo "My name is shivendra" > name.txt
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# ls -la      
+total 136
+drwx------ 14 root root  4096 Mar  6 22:02 .
+drwxr-xr-x 19 root root  4096 Feb 25 03:37 ..
+-rw-r--r--  1 root root  4705 Feb 25 03:32 .bashrc
+drwx------  6 root root  4096 Mar  6 20:51 .cache
+drwx------ 13 root root  4096 Mar  5 23:24 .config
+drwxr-xr-x  2 root root  4096 Mar  6 21:25 Desktop
+-rw-r--r--  1 root root    55 Mar  5 01:40 .dmrc
+drwxr-xr-x  3 root root  4096 Mar  5 23:53 Documents
+drwxr-xr-x  2 root root  4096 Mar  5 23:46 Downloads
+-rw-r--r--  1 root root     0 Mar  6 21:58 .face
+lrwxrwxrwx  1 root root    11 Feb 25 03:41 .face.icon -> /root/.face
+drwx------  3 root root  4096 Mar  6 20:51 .gnupg
+-rwxrwxrwx  1 root root     0 Mar  6 21:57 hello.txt
+-rw-------  1 root root     0 Mar  5 01:40 .ICEauthority
+drwxr-xr-x  3 root root  4096 Mar  5 01:40 .local
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Music
+-rw-r--r--  1 root root    21 Mar  6 22:02 name.txt
+drwxr-xr-x  2 root root  4096 Mar  5 23:49 Pictures
+-rw-r--r--  1 root root   148 Feb 16 04:16 .profile
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Public
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Templates
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-clipboard.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-display-svga-x11.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-draganddrop.pid
+-rw-r-----  1 root root     4 Mar  6 20:51 .vboxclient-seamless.pid
+drwxr-xr-x  2 root root  4096 Mar  5 01:40 Videos
+-rw-------  1 root root    49 Mar  6 20:51 .Xauthority
+-rw-------  1 root root  7549 Mar  6 22:01 .xsession-errors
+-rw-------  1 root root 12703 Mar  6 00:15 .xsession-errors.old
+-rw-------  1 root root   881 Mar  6 00:13 .zsh_history
+-rw-r--r--  1 root root  8381 Feb 25 03:32 .zshrc
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# cat /etc/passwd                                                        
+root:x:0:0:root:/root:/usr/bin/zsh
+daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+bin:x:2:2:bin:/bin:/usr/sbin/nologin
+sys:x:3:3:sys:/dev:/usr/sbin/nologin
+sync:x:4:65534:sync:/bin:/bin/sync
+games:x:5:60:games:/usr/games:/usr/sbin/nologin
+man:x:6:12:man:/var/cache/man:/usr/sbin/nologin
+lp:x:7:7:lp:/var/spool/lpd:/usr/sbin/nologin
+mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
+news:x:9:9:news:/var/spool/news:/usr/sbin/nologin
+uucp:x:10:10:uucp:/var/spool/uucp:/usr/sbin/nologin
+proxy:x:13:13:proxy:/bin:/usr/sbin/nologin
+www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin
+backup:x:34:34:backup:/var/backups:/usr/sbin/nologin
+list:x:38:38:Mailing List Manager:/var/list:/usr/sbin/nologin
+irc:x:39:39:ircd:/run/ircd:/usr/sbin/nologin
+gnats:x:41:41:Gnats Bug-Reporting System (admin):/var/lib/gnats:/usr/sbin/nologin
+nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin
+_apt:x:100:65534::/nonexistent:/usr/sbin/nologin
+systemd-timesync:x:101:101:systemd Time Synchronization,,,:/run/systemd:/usr/sbin/nologin
+systemd-network:x:102:103:systemd Network Management,,,:/run/systemd:/usr/sbin/nologin
+systemd-resolve:x:103:104:systemd Resolver,,,:/run/systemd:/usr/sbin/nologin
+mysql:x:104:110:MySQL Server,,,:/nonexistent:/bin/false
+tss:x:105:111:TPM software stack,,,:/var/lib/tpm:/bin/false
+strongswan:x:106:65534::/var/lib/strongswan:/usr/sbin/nologin
+ntp:x:107:113::/nonexistent:/usr/sbin/nologin
+messagebus:x:108:114::/nonexistent:/usr/sbin/nologin
+redsocks:x:109:115::/var/run/redsocks:/usr/sbin/nologin
+rwhod:x:110:65534::/var/spool/rwho:/usr/sbin/nologin
+iodine:x:111:65534::/run/iodine:/usr/sbin/nologin
+miredo:x:112:65534::/var/run/miredo:/usr/sbin/nologin
+_rpc:x:113:65534::/run/rpcbind:/usr/sbin/nologin
+usbmux:x:114:46:usbmux daemon,,,:/var/lib/usbmux:/usr/sbin/nologin
+tcpdump:x:115:120::/nonexistent:/usr/sbin/nologin
+rtkit:x:116:121:RealtimeKit,,,:/proc:/usr/sbin/nologin
+sshd:x:117:65534::/run/sshd:/usr/sbin/nologin
+statd:x:118:65534::/var/lib/nfs:/usr/sbin/nologin
+postgres:x:119:123:PostgreSQL administrator,,,:/var/lib/postgresql:/bin/bash
+avahi:x:120:125:Avahi mDNS daemon,,,:/run/avahi-daemon:/usr/sbin/nologin
+stunnel4:x:121:126::/var/run/stunnel4:/usr/sbin/nologin
+Debian-snmp:x:122:127::/var/lib/snmp:/bin/false
+sslh:x:123:128::/nonexistent:/usr/sbin/nologin
+nm-openvpn:x:124:129:NetworkManager OpenVPN,,,:/var/lib/openvpn/chroot:/usr/sbin/nologin
+nm-openconnect:x:125:130:NetworkManager OpenConnect plugin,,,:/var/lib/NetworkManager:/usr/sbin/nologin
+pulse:x:126:131:PulseAudio daemon,,,:/var/run/pulse:/usr/sbin/nologin
+saned:x:127:134::/var/lib/saned:/usr/sbin/nologin
+inetsim:x:128:136::/var/lib/inetsim:/usr/sbin/nologin
+colord:x:129:137:colord colour management daemon,,,:/var/lib/colord:/usr/sbin/nologin
+geoclue:x:130:138::/var/lib/geoclue:/usr/sbin/nologin
+lightdm:x:131:139:Light Display Manager:/var/lib/lightdm:/bin/false
+king-phisher:x:132:140::/var/lib/king-phisher:/usr/sbin/nologin
+skjha:x:1000:1000:skjha,,,:/home/skjha:/usr/bin/zsh
+systemd-coredump:x:999:999:systemd Core Dumper:/:/usr/sbin/nologin
+vboxadd:x:998:1::/var/run/vboxadd:/bin/false
+shivendra:x:1001:1001:Shivendra Jha,,,:/home/shivendra:/bin/bash
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# cat /etc/shadow
+root:$y$j9T$BKxm0B3t7MheDoGMLGZMe.$pkppBGBNJqWkAO4XTx63EURpKO6reCsbH4OYrWJaj42:18692:0:99999:7:::
+daemon:*:18683:0:99999:7:::
+bin:*:18683:0:99999:7:::
+sys:*:18683:0:99999:7:::
+sync:*:18683:0:99999:7:::
+games:*:18683:0:99999:7:::
+man:*:18683:0:99999:7:::
+lp:*:18683:0:99999:7:::
+mail:*:18683:0:99999:7:::
+news:*:18683:0:99999:7:::
+uucp:*:18683:0:99999:7:::
+proxy:*:18683:0:99999:7:::
+www-data:*:18683:0:99999:7:::
+backup:*:18683:0:99999:7:::
+list:*:18683:0:99999:7:::
+irc:*:18683:0:99999:7:::
+gnats:*:18683:0:99999:7:::
+nobody:*:18683:0:99999:7:::
+_apt:*:18683:0:99999:7:::
+systemd-timesync:*:18683:0:99999:7:::
+systemd-network:*:18683:0:99999:7:::
+systemd-resolve:*:18683:0:99999:7:::
+mysql:!:18683:0:99999:7:::
+tss:*:18683:0:99999:7:::
+strongswan:*:18683:0:99999:7:::
+ntp:*:18683:0:99999:7:::
+messagebus:*:18683:0:99999:7:::
+redsocks:!:18683:0:99999:7:::
+rwhod:*:18683:0:99999:7:::
+iodine:*:18683:0:99999:7:::
+miredo:*:18683:0:99999:7:::
+_rpc:*:18683:0:99999:7:::
+usbmux:*:18683:0:99999:7:::
+tcpdump:*:18683:0:99999:7:::
+rtkit:*:18683:0:99999:7:::
+sshd:*:18683:0:99999:7:::
+statd:*:18683:0:99999:7:::
+postgres:*:18683:0:99999:7:::
+avahi:*:18683:0:99999:7:::
+stunnel4:!:18683:0:99999:7:::
+Debian-snmp:!:18683:0:99999:7:::
+sslh:!:18683:0:99999:7:::
+nm-openvpn:*:18683:0:99999:7:::
+nm-openconnect:*:18683:0:99999:7:::
+pulse:*:18683:0:99999:7:::
+saned:*:18683:0:99999:7:::
+inetsim:*:18683:0:99999:7:::
+colord:*:18683:0:99999:7:::
+geoclue:*:18683:0:99999:7:::
+lightdm:*:18683:0:99999:7:::
+king-phisher:*:18683:0:99999:7:::
+skjha:$y$j9T$wHmPwhhp.JRG0yFZpOW3U1$vd7V5GlFlXI/3lwbPbHLEAWavvDyuLW30HxatX9JleD:18683:0:99999:7:::
+systemd-coredump:!*:18683::::::
+vboxadd:!:18683::::::
+shivendra:$y$j9T$rshvjZuKTbvSekHS7lzsr.$YCon2rpg5mrJflkIJi/W.K3zZb6iKXjq7dmQhBG7A.C:18693:0:99999:7:::
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# su shivendra                                                    
+┌──(shivendra㉿kali)-[/root]
+└─$ su root                                                                                                                                                                                                                                  
+Password: 
+┌──(root💀kali)-[~]
+└─# passwd root
+New password: 
+Retype new password: 
+passwd: password updated successfully
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# sudo passwd root
+New password: 
+Retype new password: 
+passwd: password updated successfully
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# history
+    1  user
+    2  uname
+    3  uname -a
+    4  pwd
+    5  cd
+    6  pwd
+    7  cd ..
+    8  pwd
+    9  ls
+   10  cd root
+   11  ls
+   12  cd /etc/
+   13  ls
+   14  ls Desktop/
+   15  ls root
+   16  cd root
+   17  cd root/
+   18  cd root
+   19  cd ..
+   20  cd root/
+   21  ls
+   22  ls Desktop
+   23  ls
+   24  ls Desktop
+   25  pwd
+   26  cd Desktop
+   27  ls
+   28  cd -/Music/
+   29  cd /Music/
+   30  cd Desktop/
+   31  cd ..
+   32  cd Desktop
+   33  ls
+   34  cd ..
+   35  cd Pictures
+   36  ls
+   37  cd ..
+   38  cd Public
+   39  ls
+   40  cd ..
+   41  ls
+   42  cd Documents
+   43  ls
+   44  mkdir shivendra
+   45  ls
+   46  cd ..
+   47  ls
+   48  mkdir Shiva
+   49  ls
+   50  rmdir Shiva
+   51  ls
+   52  ls -la
+   53  cd .cache
+   54  ls
+   55  cd ..
+   56  ls
+   57  echo "HI this is shivendra " > test.txt
+   58  ls
+   59  cp text.txt Downloads
+   60  ls
+   61  ls Downloads
+   62  ls
+   63  cd ..
+   64  cd Downloads/
+   65  ls
+   66  cd ..
+   67  ls 
+   68  cd ..
+   69  cd root/
+   70  ls
+   71  ls Downloads
+   72  ls
+   73  cd Downloads
+   74  ls
+   75  cp text.txt Downloads/
+   76  cp test.txt Downloads/
+   77  ls
+   78  cd ..
+   79  ls
+   80  mv test.txt Downloads
+   81  ls
+   82  cd Downloads
+   83  ls
+   84  rm test.txt
+   85  ls
+   86  cd ..
+   87  ls
+   88  locate bash
+   89  updatedb
+   90  passwd
+   91  man ls
+   92  ls --help
+   93  ls
+   94  mkdir shiv
+   95  ls
+   96  rm shiv
+   97  rmdir shiv
+   98  ls 
+   99  cd Documents
+  100  echo "HI this is shivendra " > text.txt
+  101  ls
+  102  cd shivendra
+  103  ls
+  104  history
+  105  clr
+  106  cl
+  107  clear
+  108  ls
+  109  cd
+  110  cd ..
+  111  cd .. 
+  112  ls
+  113  cd root
+  114  ls
+  115  passwd root
+  116  sudo passwd root
+                                                                                                                                                                                                                                             
+┌──(root💀kali)-[~]
+└─# 
